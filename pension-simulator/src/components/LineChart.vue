1a7bb0d644a8ff990f0236b566693c9fb3b1584d
sony-negi-daisuke <Negi.Daisuke@jp.sony.com>
2018-05-15 15:57:13 +0900

    Update the chart using reactiveProp mixin.

--- pension-simulator/src/components/LineChart.vue
+++ pension-simulator/src/components/LineChart.vue
@@ -3,63 +3,21 @@ import { Line, mixins } from 'vue-chartjs'
 
 // export default Bar.extend({
 export default {
-  mixins: [ Line, mixins.reactiveData ],
+  mixins: [ Line, mixins.reactiveProp ],
 //   extends: Line,
   data () {
     return {
-      // options: {
-      //   scales: {
-      //     yAxes: [
-      //       {
-      //         ticks: {
-      //           min: 0,
-      //           max: 100
-      //         }
-      //       }
-      //     ]
-      //   }
-      // }
     }
   },
   props: {
     // Reveal some properties to watch them and update the chart with them.
   },
+  watch: {
+  },
+  methods: {
+  },
   mounted () {
-    this.chartData = {
-      labels: [ 'みなと', 'なつみ', '大輔' ],
-      datasets: [
-        {
-          label: '年齢',
-          backgroundColor: '#f87979',
-          data: [ 3, 23, 37 ]
-        }
-      ]
-
-      // datasets: [
-      //   {
-      //     label: 'みなと',
-      //     data: 3
-      //   },
-      //   {
-      //     label: 'なつみ',
-      //     data: 23
-      //   },
-      //   {
-      //     label: 'だいすけ',
-      //     data: 37
-      //   }
-      // ]  
-    }
-    // this.renderChart({
-    //   labels: ['36', '37', '38', '39'],
-    //   datasets: [
-    //     {
-    //       label: 'GitHub Commits',
-    //       backgroundColor: '#f87979',
-    //       data: [40, 20, 12, 39, 10, 40, 39, 70, 40, 20, 12, 11]
-    //     }
-    //   ]
-    // })
+    this.renderChart(this.chartData, {})
   }
 }
 </script>
--- pension-simulator/src/components/LineChartTest.vue
+++ pension-simulator/src/components/LineChartTest.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <line-chart></line-chart>
+    <el-slider v-model="data1" :min="0" :max="100" />
+    <line-chart :chart-data="datacollection">
+    </line-chart>
   </div>
 </template>
 
@@ -10,6 +12,36 @@ import LineChart from './LineChart'
 export default {
   components: {
     LineChart
+  },
+  data () {
+    return {
+      data1: 50,
+      datacollection: null
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  watch: {
+    data1 () {
+      console.log('data1 has changed ' + this.data1)
+      this.fillData()
+      //this.datacollection.datasets[0].data[0] = this.data1
+    }
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: [ 'みなと', 'なつみ', '大輔' ],
+        datasets: [
+          {
+            label: '年齢',
+            backgroundColor: '#f87979',
+            data: [ this.data1, 23, 37 ]
+          }
+        ]
+      }
+    }
   }
 }
 </script>
